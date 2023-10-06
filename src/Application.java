0d98633d6b484bc28c1151d9ea3854b5ce9c676c
vihimuro <vihi1208@gmail.com>
2023-10-06 10:50:13 -0400

    feat: lesson 02 - Controlling access and writing of data

--- src/Application.java
+++ src/Application.java
@@ -1,18 +1,20 @@
+import br.com.himuro.screenmatch.models.Movie;
+
 public class Application {
     public static void main(String[] args) {
 
         Movie movie = new Movie();
-        movie.name = "The GodFather";
-        movie.releaseYear = 1970;
-        movie.minutesDuration = 180;
+        movie.setName("The Godfather");
+        movie.setReleaseYear(1972);
+        movie.setMinutesDuration(175);
 
         movie.showTechnicalSpecifications();
 
         movie.registerScore(8.6);
-        movie.registerScore(5.0);
+        movie.registerScore(9.1);
         movie.registerScore(10);
 
         movie.showTechnicalSpecifications();
-        System.out.println(movie.averageScore());
+        System.out.printf("Average: %.2f%n",movie.averageScore());
     }
 }
