67422a963d17d8d489b067a90ec1aff2c8e16580
vihimuro <vihi1208@gmail.com>
2023-10-09 09:56:35 -0400

    feat: course 03 - lesson 04 - Persisting data in files

--- src/br/com/himuro/screenmatch/application/Search.java
+++ src/br/com/himuro/screenmatch/application/Search.java
@@ -7,11 +7,14 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Search {
@@ -19,44 +22,59 @@ public class Search {
 
         String key = "insert your key";
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a movie to search: ");
-        String search = sc.nextLine();
+        String search = "";
+        List<Title> titles = new ArrayList<>();
 
-        try {
-            String urlSearch = "https://www.omdbapi.com/?t=" + search + "&apikey=" + key;
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        while (!search.equalsIgnoreCase("exit")) {
+
+            System.out.println("Enter a movie to search: ");
+            search = sc.nextLine();
+
+            if(search.equalsIgnoreCase("exit")) {
+                break;
+            }
+            try {
+                String urlSearch = "https://www.omdbapi.com/?t=" + search + "&apikey=" + key;
+
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(urlSearch))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String jsonResponse = response.body();
+                System.out.println(jsonResponse);
+
+                OmdbTitle ombdTitle = gson.fromJson(jsonResponse, OmdbTitle.class);
+                System.out.println(ombdTitle);
 
 
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(urlSearch))
-                    .build();
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+                Title title = new Title(ombdTitle);
+                System.out.println(title);
 
-            String jsonResponse = response.body();
-            System.out.println(jsonResponse);
-
-            Gson gson = new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                    .create();
-
-            OmdbTitle ombdTitle = gson.fromJson(jsonResponse, OmdbTitle.class);
-            System.out.println(ombdTitle);
-
-
-            Title title = new Title(ombdTitle);
-            System.out.println(title);
+                titles.add(title);
+            } catch (NumberFormatException e) {
+                System.out.println("An Error occurred: ");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("An Error on search occurred: ");
+                System.out.println(e.getMessage());
+            } catch (YearConversionException e) {
+                System.out.println(e.getMessage());
+            }
         }
-        catch (NumberFormatException e){
-            System.out.println("An Error occurred: ");
-            System.out.println(e.getMessage());
-        }
-        catch (IllegalArgumentException e){
-            System.out.println("An Error on search occurred: ");
-            System.out.println(e.getMessage());
-        }
-        catch (YearConversionException e){
-            System.out.println(e.getMessage());
-        }
+        System.out.println(titles);
+
+        FileWriter fileWriter = new FileWriter("titles.json");
+        fileWriter.write(gson.toJson(titles));
+        fileWriter.close();
+
+        System.out.println("Program closed.");
     }
 }
